c36ceebcdd59162ccfe143a701880fdc2381f3e7
shinyay <shinya.com@gmail.com>
2017-03-03 17:49:32 +0900

    Add countByAuthor method

--- book-service/src/main/java/com/oracle/jp/shinyay/spring/rest/BookRepository.java
+++ book-service/src/main/java/com/oracle/jp/shinyay/spring/rest/BookRepository.java
@@ -1,11 +1,11 @@
 package com.oracle.jp.shinyay.spring.rest;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
+        import org.springframework.data.jpa.repository.Query;
+        import org.springframework.data.repository.PagingAndSortingRepository;
+        import org.springframework.data.repository.query.Param;
+        import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.List;
+        import java.util.List;
 
 public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
 
@@ -16,6 +16,8 @@ public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
 
     @Query("SELECT b FROM Book b WHERE b.published BETWEEN :startYear AND :endYear ORDER BY b.published")
     public List<Book> getBooksBetweenYears(@Param("startYear") int startYear, @Param("endYear") int endYear);
+
+    public List<Book> findByTitleContaining(@Param("title") String title);
 
     public int countByAuthor(@Param("author") String author);
 }
